3528a1ac29e12e138ad385de2bf8b792178ecbe6
RihoHayashida <140701356+RihoHayashida@users.noreply.github.com>
2023-07-27 18:46:13 +0900

    Update streamlit_app.py
    added list to pick fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
@@ -12,7 +13,7 @@ streamlit.text('🥑🍞Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # ここに選択リストを置き、含めたい果物を選択できるようにしましょう。
-streamlit.multiselect("Pick some Fruits:", list(my_fruit_list.index)) 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # ページにテーブルを表示します。
 streamlit.dataframe(my_fruit_list)
