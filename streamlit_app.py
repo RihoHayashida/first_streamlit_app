2a866fa9000ce2e4bbd82304b954ef2b3d8ab5d7
RihoHayashida <140701356+RihoHayashida@users.noreply.github.com>
2023-07-30 14:30:29 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,10 +5,6 @@ import pandas
 # リクエストを使用すると、HTTP/1.1 リクエストを非常に簡単に送信できます。
 # URL にクエリ文字列を手動で追加したり、POST データをフォーム エンコードしたりする必要はありません。
 import requests
-Fruityvice_response =requests.get("https://fruityvice.com/api/fruit/watermelon") 
-streamlit.header("Fruityvice Fruit Advice!")
-streamlit.text(fruityvice_response)
-streamlit.text(fruityvice_response.jspn())
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -28,3 +24,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # ページにテーブルを表示します。
 streamlit.dataframe(fruits_to_show)
+
+Fruityvice_response =requests.get("https://fruityvice.com/api/fruit/watermelon") 
+streamlit.header("Fruityvice Fruit Advice!")
+streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.jspn())
