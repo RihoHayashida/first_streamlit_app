ed8bdd068f589aa650c39439573184920b9eb3b0
RihoHayashida <140701356+RihoHayashida@users.noreply.github.com>
2023-07-31 12:47:29 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,13 +62,14 @@ my_cur = my_cnx.cursor()
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_cur.execute("select * from fruit_load_list")
 
-my_data_row = my_cur.fetchone()
+# my_data_rows = my_cur.fetchone()
+my_data_rows = my_cur.fetchall()
 
 # streamlit.text("Hello from Snowflake:")
 streamlit.header("The fruit load list contains: ")
 
 # streamlit.text(my_data_row)
-streamlit.dataframe(my_data_row)
+streamlit.dataframe(my_data_rows)
 
 # テキスト入力
 add_my_fruit = streamlit.text_input('What fruit would you like add?','Apple')
