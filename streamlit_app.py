bfe1e2c39fa0a11b5187d3425155c7e3cd1be602
RihoHayashida <140701356+RihoHayashida@users.noreply.github.com>
2023-07-31 12:30:44 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,12 @@
 import streamlit
+
+#############################################################################################################################
+
 import pandas
-
-# リクエスト：https://docs.python-requests.org/en/latest/
-# リクエストを使用すると、HTTP/1.1 リクエストを非常に簡単に送信できます。
-# URL にクエリ文字列を手動で追加したり、POST データをフォーム エンコードしたりする必要はありません。
-import requests
-
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
+
+#############################################################################################################################
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
@@ -17,6 +16,8 @@ streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+#############################################################################################################################
 
 # ここに選択リストを置き、スムージーに入れたい果物を選択できるようにしましょう。初期選択はアボカドとイチゴです。
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
@@ -31,25 +32,44 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
+#############################################################################################################################
+
+# リクエスト：https://docs.python-requests.org/en/latest/
+# リクエストを使用すると、HTTP/1.1 リクエストを非常に簡単に送信できます。
+# URL にクエリ文字列を手動で追加したり、POST データをフォーム エンコードしたりする必要はありません。
+import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
-# streamlit.text(fruityvice_response)
-# streamlit.text(fruityvice_response.json())
+#############################################################################################################################
 
+# use json data
 # normalize json data
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # create table by use normalized json data
 streamlit.dataframe(fruityvice_normalized)
 
+# streamlit.text(fruityvice_response)
+# streamlit.text(fruityvice_response.json())
+
+#############################################################################################################################
+
+# connect to snowflake
 # requirements.txtファイルは、プロジェクトで使用する予定のライブラリをStreamlitに伝え、事前にライブラリを追加できるようにします。
 # 以下に示す行は、プロジェクトに追加したライブラリを使用するように py ファイルに指示します。 
 import snowflake.connector
 
-# connect to snowflake
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+
+# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute("select * from fruit_load_list")
+
 my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
+
+# streamlit.text("Hello from Snowflake:")
+streamlit.text("The fruit load list contains: ")
+
 streamlit.text(my_data_row)
+
+#############################################################################################################################
 
